d9de18e02af957786faefa5d445257c4102ba284
ash199909 <ayshaitdevelopment@gmail.com>
2024-04-12 15:19:59 +0530

    Add Sun, CrescentMoonContainer, StarWidget, CloudWidget, and GradientBackground classes

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,6 +1,7 @@
 import 'dart:math' as math;
 
 import 'package:animatedlogin/main.dart';
+import 'package:animatedlogin/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 
@@ -127,40 +128,7 @@ class _SignupScreenState extends State<SignupScreen>
       ),
       body: Stack(
         children: [
-          AnimatedCrossFade(
-            crossFadeState: Theme.of(context).brightness == Brightness.dark
-                ? CrossFadeState.showFirst
-                : CrossFadeState.showSecond,
-            duration: const Duration(seconds: 1),
-            firstChild: Container(
-              height: double.maxFinite,
-              width: double.maxFinite,
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Colors.black,
-                    Color.fromARGB(255, 67, 29, 172),
-                  ],
-                ),
-              ),
-            ),
-            secondChild: Container(
-              height: double.maxFinite,
-              width: double.maxFinite,
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Color.fromARGB(255, 255, 255, 255),
-                    Color.fromARGB(255, 241, 116, 0),
-                  ],
-                ),
-              ),
-            ),
-          ),
+          const GradientBackground(),
           Animate(
               controller: _controller,
               effects: Theme.of(context).brightness == Brightness.light
@@ -201,43 +169,7 @@ class _SignupScreenState extends State<SignupScreen>
 
                 ///
               )),
-          Animate(
-              controller: _controller,
-              effects: Theme.of(context).brightness == Brightness.dark
-                  ? const [
-                      FadeEffect(begin: 0, end: 1.0),
-                      SlideEffect(
-                          curve: Curves.easeOut,
-                          begin: Offset(3, 0),
-                          end: Offset(0, 0),
-                          duration: Duration(seconds: 1)),
-                      SlideEffect(
-                          curve: Curves.easeOutCirc,
-                          begin: Offset(0, 3),
-                          end: Offset(0, 0),
-                          duration: Duration(seconds: 1))
-                    ]
-                  : const [
-                      FadeEffect(begin: 0, end: 1.0),
-                      SlideEffect(
-                          curve: Curves.easeOut,
-                          begin: Offset(-3, 0),
-                          end: Offset(0, 0),
-                          duration: Duration(seconds: 1)),
-                      SlideEffect(
-                          curve: Curves.easeOutCirc,
-                          begin: Offset(0, 3),
-                          end: Offset(0, 0),
-                          duration: Duration(seconds: 1))
-                    ],
-              child: const Align(
-                  alignment: Alignment.topCenter,
-                  child:
-                      //container sun
-                      Sun()
-
-                  //
-                  )),
+          StarWidget(controller: _controller),
           SizedBox(
             height: 150,
             width: double.maxFinite,
@@ -279,49 +211,7 @@ class _SignupScreenState extends State<SignupScreen>
               ],
             ),
           ),
-          Animate(
-            // delay: const Duration(milliseconds: 3000),
-            controller: _controller2,
-            effects: const [
-              SlideEffect(
-                duration: Duration(milliseconds: 1000),
-                begin: Offset(3, 0),
-                end: Offset(0, 0),
-              )
-            ],
-            child: const SizedBox(
-              height: 150,
-              width: double.maxFinite,
-              child: Stack(
-                children: [
-                  Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Icon(
-                      Icons.cloud,
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      size: 55,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: Icon(
-                      Icons.cloud,
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      size: 66,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment(.34, 1.7),
-                    child: Icon(
-                      Icons.cloud,
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      size: 70,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
+          CloudWidget(controller2: _controller2),
           Visibility(
             visible: true,
             child: Container(
@@ -410,104 +300,6 @@ class _SignupScreenState extends State<SignupScreen>
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-///sun container
-
-class Sun extends StatefulWidget {
-  const Sun({
-    super.key,
-  });
-
-  @override
-  State<Sun> createState() => _SunState();
-}
-
-class _SunState extends State<Sun> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-
-    _controller =
-        AnimationController(vsync: this, duration: const Duration(seconds: 2))
-          ..forward()
-          ..repeat(reverse: true);
-
-    _animation = Tween<double>(begin: 30, end: 100).animate(_controller);
-
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-        animation: _controller,
-        builder: (context, child) {
-          return SafeArea(
-            child: Container(
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.yellow,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.yellow[800]!,
-                    blurRadius: _animation.value,
-                    spreadRadius: 20,
-                    offset: const Offset(0, 0),
-                  ),
-                ],
-              ),
-              width: 100,
-              height: 100,
-            ),
-          );
-        });
-  }
-}
-
-///moon container
-///
-///
-
-class CrescentMoonContainer extends StatelessWidget {
-  final Color color;
-
-  CrescentMoonContainer({required this.color});
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: SizedBox(
-        height: 100,
-        width: 100,
-        child: Transform.rotate(
-            angle: math.pi / -5,
-            child: Icon(
-              Icons.nightlight_round_outlined,
-              color: color,
-              size: 100,
-              shadows: [
-                BoxShadow(
-                  color: const Color.fromARGB(255, 255, 255, 255)!,
-                  blurRadius: 20,
-                  spreadRadius: 20,
-                  offset: const Offset(0, 0),
-                ),
-              ],
-            )),
       ),
     );
   }
