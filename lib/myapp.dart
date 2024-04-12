4081ea70434221b2cc1f9449e6b05ff2f10f9f4c
ash199909 <ayshaitdevelopment@gmail.com>
2024-04-12 16:08:03 +0530

    Refactor SignupScreen widget and add theme change functionality

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -5,9 +5,12 @@ import 'package:animatedlogin/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 
+//////////////////////////
+/// Constants
 Color kblack = Colors.black;
 const Color kwhite = Colors.white;
 const Color kfire = Color.fromARGB(255, 255, 87, 15);
+//////////////////////////
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({Key? key}) : super(key: key);
@@ -77,6 +80,44 @@ class _SignupScreenState extends State<SignupScreen>
     );
   }
 
+  void changeTheme(bool isDarkMode) {
+    if (isDarkMode) {
+      _controller.forward();
+      _controller2.forward();
+    } else {
+      _controller.reverse();
+      _controller2.reverse();
+    }
+
+    final value = Theme.of(context).brightness == Brightness.light;
+    setState(() {
+      MyApp.of(context)!.changeTheme(value ? ThemeMode.dark : ThemeMode.light);
+    });
+  }
+
+  Widget buildStarWidget(Alignment alignment) {
+    return Align(
+      alignment: alignment,
+      child: const Icon(
+        Icons.star,
+        color: Color.fromARGB(255, 255, 255, 255),
+        size: 15,
+      ).animate(controller: _controller2).scaleXY(
+            begin: 1.0,
+            end: 0.1,
+          ),
+    );
+  }
+
+  Widget buildTextField(String label) {
+    return TextField(
+      decoration: buildInputDecoration(label),
+      obscureText: label == "Password",
+    ).animate().fadeIn(
+          begin: 0.0,
+        );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -89,17 +130,7 @@ class _SignupScreenState extends State<SignupScreen>
             duration: const Duration(seconds: 1),
             child: Theme.of(context).brightness == Brightness.dark
                 ? IconButton(
-                    onPressed: () {
-                      _controller.forward();
-                      _controller2.forward();
-                      final value =
-                          Theme.of(context).brightness == Brightness.light;
-                      setState(() {
-                        // Change the brightness based on the switch value
-                        MyApp.of(context)!.changeTheme(
-                            value ? ThemeMode.dark : ThemeMode.light);
-                      });
-                    },
+                    onPressed: () => changeTheme(true),
                     icon: Transform.rotate(
                       angle: math.pi / -5,
                       child: const Icon(
@@ -108,17 +139,7 @@ class _SignupScreenState extends State<SignupScreen>
                       ),
                     ))
                 : IconButton(
-                    onPressed: () {
-                      _controller.reverse();
-                      _controller2.reverse();
-                      final value =
-                          Theme.of(context).brightness == Brightness.light;
-                      setState(() {
-                        // Change the brightness based on the switch value
-                        MyApp.of(context)!.changeTheme(
-                            value ? ThemeMode.dark : ThemeMode.light);
-                      });
-                    },
+                    onPressed: () => changeTheme(false),
                     icon: const Icon(
                       Icons.light_mode,
                       color: kfire,
@@ -126,6 +147,9 @@ class _SignupScreenState extends State<SignupScreen>
           ),
         ],
       ),
+
+      ///body
+      ///
       body: Stack(
         children: [
           const GradientBackground(),
@@ -175,39 +199,9 @@ class _SignupScreenState extends State<SignupScreen>
             width: double.maxFinite,
             child: Stack(
               children: [
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: const Icon(
-                    Icons.star,
-                    color: Color.fromARGB(255, 255, 255, 255),
-                    size: 15,
-                  ).animate(controller: _controller2).scaleXY(
-                        begin: 1.0,
-                        end: 0.1,
-                      ),
-                ),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: const Icon(
-                    Icons.star,
-                    color: Color.fromARGB(255, 255, 255, 255),
-                    size: 15,
-                  ).animate(controller: _controller2).scaleXY(
-                        begin: 1.0,
-                        end: 0.1,
-                      ),
-                ),
-                Align(
-                  alignment: const Alignment(-.34, 2),
-                  child: const Icon(
-                    Icons.star,
-                    color: Color.fromARGB(255, 255, 255, 255),
-                    size: 15,
-                  ).animate(controller: _controller2).scaleXY(
-                        begin: 1.0,
-                        end: 0.1,
-                      ),
-                ),
+                buildStarWidget(Alignment.centerLeft),
+                buildStarWidget(Alignment.bottomRight),
+                buildStarWidget(const Alignment(-.34, 2)),
               ],
             ),
           ),
@@ -257,24 +251,11 @@ class _SignupScreenState extends State<SignupScreen>
                       ),
                     ],
                   ),
-                  TextField(
-                    decoration: buildInputDecoration("Username"),
-                  ).animate().fadeIn(
-                        begin: 0.0,
-                      ),
+                  buildTextField("Username"),
                   const SizedBox(height: 16.0),
-                  TextField(
-                    decoration: buildInputDecoration("Email"),
-                  ).animate().fadeIn(
-                        begin: 0.0,
-                      ),
+                  buildTextField("Email"),
                   const SizedBox(height: 16.0),
-                  TextField(
-                    decoration: buildInputDecoration("Password"),
-                    obscureText: true,
-                  ).animate().fadeIn(
-                        begin: 0.0,
-                      ),
+                  buildTextField("Password"),
                   const SizedBox(height: 16.0),
                   SizedBox(
                     width: double.maxFinite,
